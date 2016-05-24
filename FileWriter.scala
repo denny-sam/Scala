1e8a8a3a681f12652e2bdd823f42f10a382fbf0a
Vipul Vaibhaw <vaibhaw.vipul@gmail.com>
2016-05-24 10:02:54 +0530

    Text File Writer Scala

---
+++ FileWriter.scala
@@ -0,0 +1,13 @@
+import java.io._;
+
+object FileWriter {
+  def main (args:Array[String]){
+    write("Hello");
+  }
+  def write(x:String){
+    val writer = new File("SüddeutscheZeitung.txt" )
+    val bw = new BufferedWriter(new FileWriter(writer,true))
+    val out = new PrintWriter(bw)
+    out.println(x+"\n")
+  }
+}
